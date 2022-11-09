676a9786a709bfdbcfbf696ac64d35c98282cf01
Phil Heller <philip.heller@student.kit.edu>
2022-11-09 03:56:31 +0100

    Integrate Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/html/index.html", "./src/html/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
